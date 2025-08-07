b8ab5ac2bc73632f2549719721a55397bb538a40
priyammaz <priyammazumdar1@gmail.com>
2025-08-06 21:18:13 -0500

    fixed librispeech filepath

--- PyTorch for Audio/Wav2Vec2/data/prepare_data.sh
+++ PyTorch for Audio/Wav2Vec2/data/prepare_data.sh
@@ -3,13 +3,13 @@ path_to_store="data/"
 
 train_clean_100="train-clean-100"
 train_clean_360="train-clean-360"
-train_clean_500="train-clean-500"
+train_other_500="train-other-500"
 dev_clean="dev-clean"
 test_clean="test-clean"
 
 wget https://www.openslr.org/resources/12/$train_clean_100.tar.gz -P $path_to_store
 wget https://www.openslr.org/resources/12/$train_clean_360.tar.gz -P $path_to_store
-wget https://www.openslr.org/resources/12/$train_clean_500.tar.gz -P $path_to_store
+wget https://www.openslr.org/resources/12/$train_other_500.tar.gz -P $path_to_store
 wget https://www.openslr.org/resources/12/$dev_clean.tar.gz -P $path_to_store
 wget https://www.openslr.org/resources/12/$test_clean.tar.gz -P $path_to_store
 
