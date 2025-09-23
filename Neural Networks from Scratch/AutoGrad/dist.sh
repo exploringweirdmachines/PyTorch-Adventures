42131cd052d505f1f2095b6245dde7c003c5388b
priyammaz <priyammazumdar1@gmail.com>
2025-09-23 16:06:10 -0500

    cleanup and remove files

--- Neural Networks from Scratch/AutoGrad/dist.sh
+++
@@ -1,3 +0,0 @@
-python -m mytorch.distributed.launch \
-    --num_gpus 2 \
-    --training_script train_ddp_mnist.py
--- Neural Networks from Scratch/AutoGrad/run_ddp_mnist.sh
+++
@@ -1,5 +0,0 @@
-# python mytorch/distributed/launch.py --num_gpus 2 --training_script train_ddp_mnist.py \
-#     --batch_size 32 \
-#     --lr 0.001
-
-python mytorch/distributed/launch.py --num_gpus 2 --training_script train_ddp_mnist.py --batch_size 32 --lr 0.001
