f81f67a7073baf09284a339a8ae4c9b3f32f2068
priyammaz <priyammazumdar1@gmail.com>
2025-10-16 19:29:21 -0500

    added in unified trainer and inference script

--- Neural Networks from Scratch/AutoGrad/train_gpt2.py
+++ Neural Networks from Scratch/AutoGrad/train_gpt2.py
@@ -1,15 +1,13 @@
 import os
 import argparse
 import numpy as np
-import cupy as cp
 import mytorch
 import mytorch.nn as nn
 import mytorch.optim as optim
 from models.gpt2 import GPT2, GPT2Config
 from miniddp.accelerate import Accelerator
 from tqdm import tqdm
-
-GPT2_VOCAB_SIZE = 50257
+import pickle
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Train GPT2 with MyTorch")
@@ -19,23 +17,23 @@ def parse_args():
     parser.add_argument("--run_name", type=str)
 
     ### Checkpointing Config ###
-    parser.add_argument("--working_directory", type=str, required=True)
-    parser.add_argument("--checkpoint_iter", type=int, default=10000)
+    parser.add_argument("--working_directory", type=str, default="work_dir")
+    parser.add_argument("--checkpoint_iterations", type=int, default=10000)
+    parser.add_argument("--always_save_checkpoint", action="store_true")
     parser.add_argument("--resume_from_checkpoint", type=str)
 
     ### Model Config ###
     parser.add_argument("--context_length", type=int, default=1024)
-    parser.add_argument("--embed_dim", type=int, default=768)
-    parser.add_argument("--num_heads", type=int, default=12)
-    parser.add_argument("--num_blocks", type=int, default=12)
+    parser.add_argument("--model_size", type=str, choices=("small", "base"), default="base")
     parser.add_argument("--dropout_p", type=float, default=0.)
-    parser.add_argument("--mlp_ratio", type=int, default=4)
     parser.add_argument("--use_bias", action="store_true")
     parser.add_argument("--fused", action="store_true")
 
     ### Training Config ###
-    parser.add_argument("--data_path", type=str, required=True)
+    parser.add_argument("--path_to_data", type=str, required=True)
     parser.add_argument("--train_iterations", type=int, default=150000)
+    parser.add_argument("--eval_interval", type=int, default=2000)
+    parser.add_argument("--eval_iterations", type=int, default=200)
     parser.add_argument("--batch_size", type=int, default=256)
     parser.add_argument("--max_lr", type=float, default=6e-4)
     parser.add_argument("--min_lr", type=float, default=6e-5)
@@ -50,72 +48,119 @@ def parse_args():
     ### Logging Config ###
     parser.add_argument("--log_iter", type=int, default=100)
     parser.add_argument("--log_wandb", action="store_true")
-    
 
     args = parser.parse_args()
 
     return args
 
-### Load Arguments ###
 args = parse_args()
 
-### LOAD DATASET ###
+### Load Model Variant ###
+if args.model_size == "small":
+    embed_dim = 384
+    num_heads = 6
+    num_blocks = 6
+    mlp_ratio = 4
+elif args.model_size == "base":
+    embed_dim = 768
+    num_heads = 12
+    num_blocks = 12
+    mlp_ratio = 4
+
+### Check Dataset ###
+if not os.path.exists(args.path_to_data):
+    raise ValueError("Verify your path to data, it should contain "
+                     "a train.bin, val.bin and optionally a tokenizer.pkl")
+
+required_files = ["train.bin", "val.bin"]
+optional_files = ["tokenizer.pkl"]
+
+for fname in required_files:
+    fpath = os.path.join(args.path_to_data, fname)
+    if not os.path.exists(fpath):
+        raise ValueError(f"Missing required file: {fname} in {args.path_to_data}")
+
+# Optionally check tokenizer
+tokenizer_path = os.path.join(args.path_to_data, "tokenizer.pkl")
+tokenizer_exists = os.path.exists(tokenizer_path)
+if not tokenizer_exists:
+    print("No tokenizer.pkl found, assuming default gpt2 tokenizer!")
+    vocab_size = 50257
+else:
+    with open(tokenizer_path, "rb") as f:
+        tokenizer_meta = pickle.load(f)
+
+    vocab_size = tokenizer_meta["vocab_size"]
+
+### DataLoader ###
 def get_batch(train=True):
 
     if train:
-        data = np.memmap(os.path.join(args.data_path, 'train.bin'), dtype=np.uint16, mode='r')
+        data = np.memmap(os.path.join(args.path_to_data, 'train.bin'), dtype=np.uint16, mode='r')
     else:
-        data = np.memmap(os.path.join(args.data_path, 'test.bin'), dtype=np.uint16, mode='r')
+        data = np.memmap(os.path.join(args.path_to_data, 'val.bin'), dtype=np.uint16, mode='r')
 
     start_idx = np.random.randint(low=0, high=len(data) - args.context_length - 1, size=(args.batch_size//args.gradient_accumulation_steps))
     x = np.stack([data[i:i+args.context_length] for i in start_idx])
     y = np.stack([data[i+1:i+args.context_length+1] for i in start_idx])
-
+    
     return mytorch.Tensor(x), mytorch.Tensor(y)
 
-### INIT ACCELERATOR ###
+### Create Checkpoint Directory ###
 path_to_experiment = os.path.join(args.working_directory, args.project_name)
 if args.run_name is not None:
     path_to_experiment = os.path.join(path_to_experiment, args.run_name)
+os.makedirs(path_to_experiment, exist_ok=True)
 
-accelerator = Accelerator(project_dir=path_to_experiment, 
-                          gradient_accumulation_steps=args.gradient_accumulation_steps,
-                          mixed_precision=args.mixed_precision)
+### Init DDP ###
+accelerator = Accelerator(gradient_accumulation_steps=args.gradient_accumulation_steps,
+                          mixed_precision=args.mixed_precision,
+                          log_wand=args.log_wandb)
+
+### Config to store model information (so we can load precise setup for inference!) ###
+training_config = {
+                    "vocab_size": vocab_size,
+                    "context_length": args.context_length,
+                    "embed_dim": embed_dim,
+                    "num_heads": num_heads,
+                    "num_blocks": num_blocks,
+                    "mlp_ratio": mlp_ratio,
+                    "use_bias": args.use_bias,
+                    "dropout": args.dropout_p,
+                    "batch_size": args.batch_size,
+                    "grad_accumulation_steps": args.gradient_accumulation_steps,
+                    "max_lr": args.max_lr,
+                    "weight_decay": args.weight_decay,
+                    "adam_beta1": args.beta1,
+                    "adam_beta2": args.beta2,
+                    "mixed_precision": args.mixed_precision,
+                    "path_to_experiment": path_to_experiment,
+                    "path_to_tokenizer": tokenizer_path if tokenizer_exists else None
+                }
+
+if accelerator.is_main_process:
+    with open(os.path.join(path_to_experiment, "model_meta.pkl"), "wb") as f:
+            pickle.dump(training_config, f)
 
 if args.log_wandb:
-    training_config = {
-                        "context_length": args.context_length,
-                        "embed_dim": args.embed_dim,
-                        "num_heads": args.num_heads,
-                        "num_blocks": args.num_blocks,
-                        "dropout": args.dropout_p,
-                        "batch_size": args.batch_size,
-                        "grad_accumulation_steps": args.gradient_accumulation_steps,
-                        "max_lr": args.max_lr,
-                        "weight_decay": args.weight_decay,
-                        "adam_beta1": args.beta1,
-                        "adam_beta2": args.beta2,
-                        "mixed_precision": args.mixed_precision
-                    }
-
     accelerator.init_tracker(project_name=args.project_name, 
                              run_name=args.run_name,
                              config=training_config)
 
 ### Load Model ###
-config = GPT2Config(
-        vocab_size=GPT2_VOCAB_SIZE,
-        max_seq_len=args.context_length,
-        embed_dim=args.embed_dim, 
-        num_heads=args.num_heads,
-        num_blocks=args.num_blocks, 
-        attn_dropout_p=args.dropout_p, 
-        mlp_dropout_p=args.dropout_p, 
-        mlp_ratio=args.mlp_ratio,
-        use_bias=args.use_bias, 
-        use_fused_ops=args.fused
-    )
-model = GPT2(config)
+gpt2config = GPT2Config(
+    vocab_size=vocab_size, 
+    max_seq_len=args.context_length,
+    embed_dim=embed_dim, 
+    num_heads=num_heads, 
+    num_blocks=num_blocks, 
+    attn_dropout_p=args.dropout_p, # Only works for non-fused attn 
+    mlp_dropout_p=args.dropout_p, 
+    mlp_ratio=mlp_ratio, 
+    use_bias=args.use_bias, 
+    use_fused_ops=args.fused
+)
+model = GPT2(gpt2config)
 
 total_params = 0
 for param in model.parameters():
@@ -144,7 +189,14 @@ model, optimizer = accelerator.prepare(model, optimizer)
 if args.resume_from_checkpoint is not None:
 
     ### Grab path to checkpoint ###
-    path_to_checkpoint = os.path.join(path_to_experiment, args.resume_from_checkpoint)
+
+    ### This is if we pass in a full path to checkpoint ###
+    if os.path.exists(args.resume_from_checkpoint):
+        path_to_checkpoint = args.resume_from_checkpoint
+    
+    ### Otherwise we can pass the folder name in our experiment directory ###
+    else:
+        path_to_checkpoint = os.path.join(path_to_experiment, args.resume_from_checkpoint)
     
     ### Load our State (model and optimizer) ###
     accelerator.load_state(path_to_checkpoint)
@@ -160,7 +212,7 @@ else:
     completed_steps = 0
 
 ### Load Loss Function ###
-loss_fn = nn.CrossEntropyLoss()
+loss_fn = nn.CrossEntropyLoss(fused=args.fused)
 
 ### Train Model ###
 pbar = tqdm(range(args.train_iterations), 
@@ -189,40 +241,69 @@ for iter in range((args.train_iterations - completed_steps) * args.gradient_accu
 
     ### Accelerator tracks when accumulation is done, the flag is just sync_grad ###
     if accelerator.sync_grad:
-
+        
+        ### One full accumulation complete ###
         completed_steps += 1
         pbar.update(1)
 
         ### Update Scheduler ###
         scheduler.step()
 
-        # Gather metrics across GPUs
+        ### Gather metrics across GPUs
         if completed_steps % args.log_iter == 0:
-            loss_val = accelerator.gather_for_metrics(loss)
 
-            accuracy_val = accelerator.gather_for_metrics(
-                (logits.argmax(dim=-1).reshape(-1) == targets.reshape(-1)).sum() / len(targets.reshape(-1))
-            )
+            ### Gather (no-op if we are on single GPU) ###
+            loss = accelerator.gather_for_metrics(loss)
 
             ### Grab our stored grad_norm for checking on model health ###
-            grad_norm = accelerator.grad_norm
-            log_statement = f"Iter {completed_steps}, Loss: {loss_val:.4f}, LR: {scheduler.get_last_lr():.2e}, Acc: {accuracy_val:.4f}"
-            if grad_norm is not None:
-                log_statement += f" Grad Norm: {grad_norm:.3f}"
+            log_statement = f"Iter {completed_steps}, Loss: {loss:.4f}, LR: {scheduler.get_last_lr():.2e}"
+            if accelerator.grad_norm is not None:
+                log_statement += f" Grad Norm: {accelerator.grad_norm:.3f}"
 
+            ### Print to Console ###
             accelerator.print(log_statement)
 
+            ### Log with Wandb if enabled ###
+            if args.log_wandb:  
+                logging_dict = {"loss": loss_val, "lr": scheduler.get_last_lr()}
+                if accelerator.grad_norm is not None:
+                    logging_dict["grad_norm"] = accelerator.grad_norm 
+                accelerator.log(logging_dict, step=completed_steps)
+
+        if completed_steps % args.checkpoint_iterations == 0 and args.always_save_checkpoint:
+            accelerator.save_state(os.path.join(path_to_experiment, f"checkpoint_{completed_steps}"))
+
+        if completed_steps % args.eval_interval == 0:
+            
+            accelerator.print("Evaluating!")
+            model.eval()
+
+            val_losses = []
+
+            for _ in range(args.eval_iterations):
+
+                inputs, targets = get_batch(train=False)
+                inputs, targets = inputs.to(accelerator.device), targets.to(accelerator.device)
+
+                # Forward pass
+                with mytorch.no_grad():
+                    logits = model(inputs)
+
+                ### Compute/Gather Loss ###
+                loss = loss_fn(logits, targets)
+                loss_val = accelerator.gather_for_metrics(loss)
+                val_losses.append(loss_val)
+
+            ### Log Loss ###
+            val_losses = np.mean(val_losses)
+            accelerator.print("Validation Loss:", val_losses)
             if args.log_wandb:
-                accelerator.log({"loss": loss_val, "lr": scheduler.get_last_lr(), "grad_norm": grad_norm}, step=completed_steps)
+                logging_dict = {"val_loss": val_losses}
+                accelerator.log(logging_dict, step=completed_steps)
 
-            if completed_steps % args.checkpoint_iter == 0 :
-                accelerator.save_state(os.path.join(path_to_experiment, f"checkpoint_{completed_steps}"))
+            ### Set back into Training Mode ###
+            model.train()
 
-
-# ### Save Model ###
-# accelerator.save_state(os.path.join(path_to_experiment, f"final_checkpoint"))
-
-# # End training
-# accelerator.end_training()  
-        
-    
+### Save final checkpoint once done ! ###
+accelerator.save_state(os.path.join(path_to_experiment, f"final_checkpoint"))
+accelerator.end_training()
