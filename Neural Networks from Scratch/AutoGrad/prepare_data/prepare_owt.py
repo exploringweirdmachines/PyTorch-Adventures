d511bb5bb74b8204f29213528b97fe095b00edde
priyammaz <priyammazumdar1@gmail.com>
2025-10-16 19:57:47 -0500

    pushing up data prep scripts

--- Neural Networks from Scratch/AutoGrad/prepare_data/prepare_owt.py
+++ Neural Networks from Scratch/AutoGrad/prepare_data/prepare_owt.py
@@ -6,31 +6,37 @@ Requires datasets==3.6.0 incase of error
 import os
 from tqdm import tqdm
 import numpy as np
-from transformers import GPT2Tokenizer
+import tiktoken
 from datasets import load_dataset # huggingface datasets
+import argparse
 
-num_proc = 24 
-num_proc_load_dataset = num_proc
-path_to_save = "/mnt/datadrive/data/owt_bin"
+parser = argparse.ArgumentParser(description="Prepare OpenWebText")
+parser.add_argument("--num_proc", type=int, default=8)
+parser.add_argument("--path_to_save", type=str, default="data/openwebtext")
+args = parser.parse_args()
+
+num_proc = args.num_proc
+num_proc_load_dataset = args.num_proc
+path_to_save = args.path_to_save
 
 if not os.path.exists(path_to_save):
     os.makedirs(path_to_save, exist_ok=True)
 
-tokenizer = GPT2Tokenizer.from_pretrained("openai-community/gpt2")
+enc = tiktoken.get_encoding("gpt2")
 
 if __name__ == '__main__':
+
     # takes 54GB in huggingface .cache dir, about 8M documents (8,013,769)
-    dataset = load_dataset("openwebtext", num_proc=num_proc_load_dataset)
+    dataset = load_dataset("openwebtext", num_proc=num_proc_load_dataset, )
+
     # owt by default only contains the 'train' split, so create a test split
     split_dataset = dataset["train"].train_test_split(test_size=0.0005, seed=2357, shuffle=True)
     split_dataset['val'] = split_dataset.pop('test') # rename the test split to val
 
-
     # we now want to tokenize the dataset. first define the encoding function (gpt2 bpe)
     def process(example):
-        ids = tokenizer.encode(example['text']) # encode_ordinary ignores any special tokens
-        ids.append(tokenizer.eos_token_id) # add the end of text token, e.g. 50256 for gpt2 bpe
-        # note: I think eot should be prepended not appended... hmm. it's called "eot" though...
+        ids = enc.encode_ordinary(example['text']) # encode_ordinary ignores any special tokens
+        ids.append(enc.eot_token) # add the end of text token, e.g. 50256 for gpt2 bpe
         out = {'ids': ids, 'len': len(ids)}
         return out
 
